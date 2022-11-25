590ef39f000d63d3655404b935c7371024486bae
RotscherK <roger.kaufmann1@students.fhnw.ch>
2022-11-25 15:43:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.all('/searchItemMatches', (req, res) => {
     const responseData = [
         {id: "1234567", searchItem: "123", title: "TestMatch1", price: "100", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Tutti", url: "www.tutti.ch"},
         {id: "1234568", searchItem: "123", title: "TestMatch2", price: "200", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Tutti", url: "www.tutti.ch"},
-        {id: "1234569", searchItem: "123", title: "TestMatch2", price: "50", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Tutti", url: "www.tutti.ch"}
+        {id: "1234569", searchItem: "123", title: "TestMatch2", price: "50", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Tutti", url: "www.tutti.ch"},
+        {id: "1234519", searchItem: "123", title: "TestMatch4", price: "50", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Anibis", url: "www.tutti.ch"}
     ]
     
     const jsonContent = JSON.stringify(responseData);
