d3b1ab76cdf62ab9319090172ddc52edc435a2a1
RotscherK <roger.kaufmann1@students.fhnw.ch>
2022-11-25 11:00:02 +0100

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+app.all('/', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
+app.all('/searchItemMatches', (req, res) => {
+
+    console.log("Just got a request!")
+    const responseData = {
+        message:"Hello, Here is you answer",
+      matchData:[
+        {searchItem: "123", title: "Lorem i", price: "100", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Tutti", url: "www.tutti.ch"},
+        {searchItem: "123", title: "TestMatch2", price: "200", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Tutti", url: "www.tutti.ch"},
+        {searchItem: "123", title: "TestMatch2", price: "50", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Tutti", url: "www.tutti.ch"}
+      ]
+    }
+    const jsonContent = JSON.stringify(responseData);
+
+    res.send(jsonContent)
+})
+app.listen(process.env.PORT || 3000)
