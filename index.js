671400788a89b3eee39ab3b8dd83581ad002c627
RotscherK <roger.kaufmann1@students.fhnw.ch>
2022-11-25 12:10:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,12 @@ app.all('/', (req, res) => {
 app.all('/searchItemMatches', (req, res) => {
 
     console.log("Just got a request!")
-    const responseData = {
-        message:"Hello, Here is you answer",
-      matchData:[
+    const responseData = [
         {searchItem: "123", title: "Lorem i", price: "100", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Tutti", url: "www.tutti.ch"},
         {searchItem: "123", title: "TestMatch2", price: "200", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Tutti", url: "www.tutti.ch"},
         {searchItem: "123", title: "TestMatch2", price: "50", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Tutti", url: "www.tutti.ch"}
-      ]
-    }
+    ]
+    
     const jsonContent = JSON.stringify(responseData);
 
     res.send(jsonContent)
