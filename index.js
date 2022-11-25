d980ab74924cfe0755684c6078086d8b806d013e
RotscherK <roger.kaufmann1@students.fhnw.ch>
2022-11-25 15:27:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.all('/searchItemMatches', (req, res) => {
 
     console.log("Just got a request!")
     const responseData = [
-        {searchItem: "123", title: "TestMatch1", price: "100", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Tutti", url: "www.tutti.ch"},
-        {searchItem: "123", title: "TestMatch2", price: "200", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Tutti", url: "www.tutti.ch"},
-        {searchItem: "123", title: "TestMatch2", price: "50", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Tutti", url: "www.tutti.ch"}
+        {"searchItem": "123", "title": "TestMatch1", "price": "100", "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", "platform": "Tutti", "url": "www.tutti.ch"},
+        {"searchItem": "123", "title": "TestMatch2", "price": "200", "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", "platform": "Tutti", "url": "www.tutti.ch"},
+        {"searchItem": "123", "title": "TestMatch2", "price": "50", "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", "platform": "Tutti", "url": "www.tutti.ch"}
     ]
     
     const jsonContent = JSON.stringify(responseData);
