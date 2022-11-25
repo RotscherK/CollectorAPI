acdc15c4be6ea5b5591986bb3a62228aa6301ec1
RotscherK <roger.kaufmann1@students.fhnw.ch>
2022-11-25 17:56:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.post('/searchItemMatches', async (req, res, next) => {
     matchesCollection = await searchMatches.get(searchItemID)
     matches = []
     
-    if(matchesCollection.props != null){
+    if(matchesCollection != null){
          Object.keys(matchesCollection.props).forEach(key => {
              console.log(key, matchesCollection.props[key]);
              matches.push(matchesCollection.props[key])
