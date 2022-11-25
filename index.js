a45e564b02ea328b1a493a970a55db0dd7249dc5
RotscherK <roger.kaufmann1@students.fhnw.ch>
2022-11-25 16:17:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,24 @@ const db = CyclicDb("amused-gold-donkeyCyclicDB")
 
 const searchMatches = db.collection("searchMatches")
 
-// create an item in collection with key "leo"
-let leo = await searchMatches.set("123", [
-    {id: "1234567", searchItem: "123", title: "TestMatch1", price: "100", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Tutti", url: "www.tutti.ch"},
-    {id: "1234568", searchItem: "123", title: "TestMatch2", price: "200", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Tutti", url: "www.tutti.ch"},
-    {id: "1234569", searchItem: "123", title: "TestMatch2", price: "50", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Tutti", url: "www.tutti.ch"},
-    {id: "1234519", searchItem: "123", title: "TestMatch4", price: "50", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Anibis", url: "www.tutti.ch"}
-])
+app.all('/init', async(req, res) => {
+    console.log("Init Load!")
+    await searchMatches.set("123", [
+        {id: "1234567", searchItem: "123", title: "TestMatch1", price: "100", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Tutti", url: "www.tutti.ch"},
+        {id: "1234568", searchItem: "123", title: "TestMatch2", price: "200", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Tutti", url: "www.tutti.ch"},
+        {id: "1234569", searchItem: "123", title: "TestMatch2", price: "50", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Tutti", url: "www.tutti.ch"},
+        {id: "1234519", searchItem: "123", title: "TestMatch4", price: "50", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Anibis", url: "www.tutti.ch"}
+    ])
+    res.send('Yo!')
+})
 
-// get an item at key "leo" from collection animals
-let item = await searchMatches.get("123")
-console.log(searchMatches)
+app.all('/showAll', async(req, res) => {
+    console.log("Just got a request!")
+    matches = await searchMatches.get(searchItemID)
+    const jsonContent = JSON.stringify(matches);
+    res.send(jsonContent)
+})
+
 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
