2b0cb25df1fc585d1ef08759e7a1d8102d011666
RotscherK <roger.kaufmann1@students.fhnw.ch>
2022-11-26 01:19:39 +0100

    start script

--- index.js
+++ index.js
@@ -14,10 +14,14 @@ const db = CyclicDb("amused-gold-donkeyCyclicDB")
 
 const searchMatches = db.collection("searchMatches")
 
+console.log("Started")
+
+
+
 app.all('/init', async(req, res) => {
     console.log("Init Load!")
     await searchMatches.set("123", [
-        {id: "1234567", searchItem: "123", title: "TestMatch1", price: "100", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Tutti", url: "www.tutti.ch"},
+        {id: "1234567", searchItem: "123", title: "TestMatch1qwe", price: "100", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Tutti", url: "www.tutti.ch"},
         {id: "1234568", searchItem: "123", title: "TestMatch2", price: "200", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Tutti", url: "www.tutti.ch"},
         {id: "1234569", searchItem: "123", title: "TestMatch2", price: "50", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Tutti", url: "www.tutti.ch"},
         {id: "1234519", searchItem: "123", title: "TestMatch4", price: "50", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknow", platform: "Anibis", url: "www.tutti.ch"}
@@ -51,7 +55,7 @@ app.post('/searchItemMatches', async (req, res, next) => {
 
                 console.log(key, matchesCollection.props[key]);
                 matches.push(matchesCollection.props[key])
-                
+
             }
 
           });
